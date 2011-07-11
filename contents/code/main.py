a4ef99344898ca0e4b62fc358548385ee15301cb
Igor Almeida <igor.contato@gmail.com>
2011-07-10 21:26:02 -0300

    dbus client (now that's what i'm talking about)
    not sure why the string won't remain in a single line (layout seems to push for a
    square aspect ratio)

--- contents/code/main.py
+++ contents/code/main.py
@@ -13,25 +13,28 @@
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from PyQt4 import QtCore
-from PyQt4.QtCore import Qt, QFileSystemWatcher
+from PyQt4.QtCore import Qt
 from PyQt4.QtGui import QGraphicsLinearLayout
 from PyKDE4.plasma import Plasma
 from PyKDE4 import plasmascript
 
 import os
 import threading
+import dbus
+from dbus.mainloop.qt import DBusQtMainLoop
 
-fifo_file = ["/home/igor/.cache/xmonad-log-plasmoid/pipe",]
-label = None
-f_obj = None #File object to read from fifo_file
+label = None #TODO not sure if I really need it global
 
 class XMonadLogPlasmoid(plasmascript.Applet):
+
+	label_signal = QtCore.pyqtSignal(str)
+	session_bus = None
 
 	def __init__(self, parent, args=None):
 		plasmascript.Applet.__init__(self, parent)
 
 	def init(self):
-		global label, f_obj
+		global label
 
 		self.setHasConfigurationInterface(False)
 #		self.setAspectRatioMode(Plasma.Square)
@@ -42,19 +45,32 @@ class XMonadLogPlasmoid(plasmascript.Applet):
 
 		self.layout = QGraphicsLinearLayout(Qt.Horizontal, self.applet)
 		label = Plasma.Label(self.applet)
-		label.setText("Hello world!")
+		label.setText("Waiting for XMonad...")
 		self.layout.addItem(label)
 		self.applet.setLayout(self.layout)
 		#self.resize(500,125)
 
-		watcher = QFileSystemWatcher(fifo_file, self.applet)
-		f_obj = open(fifo_file[0], 'r', 1)
+		self.setup_dbus()
+		self.label_signal.connect(update_label)
 
-		QtCore.QObject.connect(watcher, QtCore.SIGNAL("fileChanged(QString)"),
-			update_label)
+	def setup_dbus(self):
+		self.session_bus = dbus.SessionBus()
+		proxy = self.session_bus.get_object(
+			bus_name='org.xmonad.Log',
+			object_path='/org/xmonad/Log')
+
+		self.session_bus.add_signal_receiver(
+			handler_function=self.msg_receive,
+			signal_name='Update',
+			dbus_interface='org.xmonad.Log')
+
+	def msg_receive(self, msg):
+		self.label_signal.emit(msg)
 
 def update_label(s):
-	label.setText(f_obj.readline())
+	label.setText(s)
 
 def CreateApplet(parent):
+	DBusQtMainLoop(set_as_default=True)
 	return XMonadLogPlasmoid(parent)
+
